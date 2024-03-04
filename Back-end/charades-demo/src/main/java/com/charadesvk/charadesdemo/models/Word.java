7dc7858a0af9bdede4f39d9f322a35663e65ff3d
va_kam <tulevskiv@gmail.com>
2024-03-04 15:13:01 +0200

    Added main model - Word

---
+++ Back-end/charades-demo/src/main/java/com/charadesvk/charadesdemo/models/Word.java
@@ -0,0 +1,37 @@
+package com.charadesvk.charadesdemo.models;
+
+import org.hibernate.validator.constraints.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
+    UUID id;
+
+    @Column
+    String word;
+
+    @Column
+    String description;
+
+    @Column
+    int letterCount;
+
+    public Word (String word, String description) {
+        setWord(word);
+        setDescription(description);
+        setLetterCount(word.length());
+    }
+}
