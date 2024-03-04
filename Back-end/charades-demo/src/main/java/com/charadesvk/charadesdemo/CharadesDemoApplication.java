ef14e21d8820eb6b51c03ff7d9bf4c4b4331d128
va_kam <tulevskiv@gmail.com>
2024-03-04 14:04:55 +0200

    Initial back-end set-up

---
+++ Back-end/charades-demo/src/main/java/com/charadesvk/charadesdemo/CharadesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.charadesvk.charadesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharadesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharadesDemoApplication.class, args);
+	}
+
+}
---
+++ Back-end/charades-demo/src/test/java/com/charadesvk/charadesdemo/CharadesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charadesvk.charadesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharadesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
