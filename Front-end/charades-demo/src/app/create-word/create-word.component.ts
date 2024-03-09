f7871639a847401881c5c53fadce547ea30e5add
va_kam <tulevskiv@gmail.com>
2024-03-09 21:14:47 +0200

    Add create word component implementation.

--- Front-end/charades-demo/src/app/create-word/create-word.component.ts
+++ Front-end/charades-demo/src/app/create-word/create-word.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Word } from '../word';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { WordService } from '../word.service';
 
 @Component({
   selector: 'app-create-word',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-word.component.html',
   styleUrl: './create-word.component.css'
 })
 export class CreateWordComponent {
+  word: Word = new Word();
 
+  constructor(private service: WordService, private router: Router) {}
+
+  ngOnInit() {}
+
+  saveEmployee(){
+    this.service.createWord(this.word).subscribe( data =>{
+      console.log(data);
+      this.goToEmployeeList();
+    },
+    error => console.log(error));
+  }
+
+  goToEmployeeList(){
+    this.router.navigate(['/words']);
+  }
+
+  onSubmit() {
+    console.log(this.word);
+    this.saveEmployee();
+  }
 }
