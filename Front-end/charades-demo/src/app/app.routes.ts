fdeda71b5aedc46cee513a16a022cd5d2739afb7
va_kam <tulevskiv@gmail.com>
2024-03-09 21:00:36 +0200

    Add routing and navigation + app main view

--- Front-end/charades-demo/src/app/app.routes.ts
+++ Front-end/charades-demo/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { WordListComponent } from './word-list/word-list.component';
+import { CreateWordComponent } from './create-word/create-word.component';
+import { UpdateWordComponent } from './update-word/update-word.component';
+import { RandomWordComponent } from './random-word/random-word.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'words', component: WordListComponent},
+    {path: 'create-word', component: CreateWordComponent},
+    {path: '', redirectTo: 'words', pathMatch: 'full'},
+    {path: 'update-word/:word', component: UpdateWordComponent},
+    {path: 'random', component: RandomWordComponent},
+    {path: '**', redirectTo: 'words', pathMatch: 'full'}
+];
