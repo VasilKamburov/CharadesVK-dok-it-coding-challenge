808d6a858f3627f464dfe3f4ac0da77cbcf165a3
va_kam <tulevskiv@gmail.com>
2024-03-09 21:13:22 +0200

    Add get-random-word component implementation.

--- Front-end/charades-demo/src/app/random-word/random-word.component.ts
+++ Front-end/charades-demo/src/app/random-word/random-word.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Word } from '../word';
+import { WordService } from '../word.service';
 
 @Component({
   selector: 'app-random-word',
@@ -8,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './random-word.component.css'
 })
 export class RandomWordComponent {
+  word: Word = new Word();
 
+  constructor(private service: WordService) {}
+
+  ngOnInit() {
+    this.getRandomWord();
+  }
+
+  getRandomWord() {
+    this.service.getRandomWord().subscribe(data => {
+      this.word = data
+    }, error => console.log(error));
+  }
+
+  getNewWord() {
+    this.getRandomWord();
+  }
 }
