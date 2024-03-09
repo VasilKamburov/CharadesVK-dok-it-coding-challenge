829a125bbd6b974873160874132c9115058fafb7
va_kam <tulevskiv@gmail.com>
2024-03-09 21:10:05 +0200

    Add word-list component implementation

--- Front-end/charades-demo/src/app/word-list/word-list.component.ts
+++ Front-end/charades-demo/src/app/word-list/word-list.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Word } from '../word';
+import { Router } from '@angular/router';
+import { UUID } from 'crypto';
+import { WordService } from '../word.service';
 
 @Component({
   selector: 'app-word-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './word-list.component.html',
   styleUrl: './word-list.component.css'
 })
 export class WordListComponent {
+  words: Word[] = [];
+  title: string = "Word list";
 
+  constructor(private appService: WordService, private router: Router) {}
+
+  ngOnInit() {
+    this.getWordList();
+  }
+
+  getWordList() {
+    this.appService.getWordList().subscribe(data => {
+      this.words = data
+    });
+  }
+
+  updateWord(word: string) {
+    this.router.navigate(['update-word', word])
+  }
+
+  deleteWord(id: UUID) {
+    this.appService.deleteWord(id).subscribe(data => {
+      console.log(data);
+      this.getWordList();
+    })
+  }
 }
